322daaba94795b0d1e307125e1f4b76218bf71b0
sajib hasan <sajibhasan800080@gmail.com>
2024-11-20 21:53:45 +0600

    install typescript to the project

---
+++ source/app.js
@@ -0,0 +1,5 @@
+var num = 20;
+console.log(num);
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
---
+++ source/app.ts
@@ -0,0 +1,6 @@
+let num: number = 20;
+console.log(num)
+
+for(let i: number = 0; i < 10; i++){
+	console.log(i)
+}
